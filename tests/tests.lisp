650e1bc0f1ec0197c4fb2aa0649bfcaf87914813
Gary King <gwking@metabang.com>
2008-02-22 13:14:56 -0400

    Added more tests

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -1,3 +1,9 @@
+#|
+
+Wow, these tests sure are long winded... (and confusing)
+
+|#
+
 (in-package #:asdf-binary-locations-tests)
 
 (deftestsuite asdf-binary-locations-test () 
@@ -12,32 +18,35 @@
     (system-relative-pathname 'asdf-binary-locations "scratch/"))
    (*centralize-lisp-binaries* nil)
    (*include-per-user-information* nil)
-   (*map-all-source-files* nil)))
+   (*map-all-source-files* nil))
+  (:setup 
+   (load (system-relative-pathname 'asdf-binary-locations-test 
+				   "tests/abl-test-system.asd"))))
 
 (deftestsuite centralize-binaries (asdf-binary-locations-test)
   ())
 
 (addtest (centralize-binaries)
   try-nil
-  (let ((c (find-component-using-path :asdf-binary-locations '("dev" "main"))))
+  (let ((c (find-component-using-path :abl-test-system '("dev" "main"))))
     (ensure-same
      (namestring
       (first
        (output-files 
 	(make-instance 
 	 'compile-op
-	 :pathname (system-relative-pathname 'asdf-binary-locations "dev"))
+	 :pathname (system-relative-pathname 'abl-test-system  "dev"))
 	c)))
      (namestring
       (system-relative-pathname 
-       'asdf-binary-locations 
-       (concatenate 'string "dev/"
+       'abl-test-system 
+       (concatenate 'string "/dev/"
 		    (implementation-specific-directory-name)
 		    "/main." fasl-type))))))
 
 (addtest (centralize-binaries)
   try-true
-  (let ((c (find-component-using-path :asdf-binary-locations '("dev" "main")))
+  (let ((c (find-component-using-path :abl-test-system '("dev" "main")))
 	(*centralize-lisp-binaries* t))
     (ensure-same
      (namestring
@@ -45,16 +54,59 @@
        (output-files 
 	(make-instance 
 	 'compile-op
-	 :pathname (system-relative-pathname 'asdf-binary-locations "dev"))
+	 :pathname (system-relative-pathname 'abl-test-system  "dev"))
+	c)))
+     (namestring
+       (concatenate 
+	'string
+	(namestring *default-toplevel-directory*)
+	(implementation-specific-directory-name)
+	(namestring (system-relative-pathname 'abl-test-system  "dev"))
+	"/main." fasl-type)))))
+
+;;;;
+
+(deftestsuite map-all-source-files (asdf-binary-locations-test)
+  ())
+
+(addtest (map-all-source-files)
+  try-nil
+  (let ((c (find-component-using-path :abl-test-system '("dev" "test-source")))
+	(*map-all-source-files* nil))
+    (ensure-same
+     (namestring
+      (first
+       (output-files 
+	(make-instance 
+	 'compile-op
+	 :pathname (system-relative-pathname 'abl-test-system  "dev"))
 	c)))
      (namestring
       (system-relative-pathname 
-       'asdf-binary-locations 
-       (concatenate 
-	'string "scratch/"
-	(implementation-specific-directory-name)
-	(namestring (system-relative-pathname 'asdf-binary-locations "dev"))
-	"/main." fasl-type))))))
+       'abl-test-system 
+       (concatenate 'string "/dev/" "/test-source.xxx"))))))
+
+(addtest (map-all-source-files)
+  try-true
+  (let ((c (find-component-using-path :abl-test-system
+				      '("dev" "test-source")))
+	(*map-all-source-files* t))
+    (ensure-same
+     (namestring
+      (first
+       (output-files 
+	(make-instance 
+	 'compile-op
+	 :pathname (system-relative-pathname 'abl-test-system  "dev"))
+	c)))
+     (namestring
+      (system-relative-pathname 
+       'abl-test-system 
+       (concatenate 'string "/dev/"	
+		    (implementation-specific-directory-name)
+		    "/test-source.xxx"))))))
+
+;;;;
 
 (defmethod find-component-using-path ((system symbol) path)
   (find-component-using-path (find-system system) path))
